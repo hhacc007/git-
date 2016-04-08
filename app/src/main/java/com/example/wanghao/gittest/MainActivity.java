e65dccab3cd9ce217fb71fd076a624ae8cbb0ea6
295095305@qq.com <295095305@qq.com>
2016-04-08 15:25:13 +0800

    第二次修改

--- app/src/main/java/com/example/wanghao/gittest/MainActivity.java
+++ app/src/main/java/com/example/wanghao/gittest/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.wanghao.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findView();
+        addListener();
+    }
+
+    public void findView() {
+        button = (Button) findViewById(R.id.btn_hello);
+    }
+
+    public void addListener() {
+        button.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Hello world" , Toast.LENGTH_SHORT).show();
     }
 }
